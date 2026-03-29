d7902ee384595674c8d7ff33b8af3ab21fb6a21a
Jakub Brochocki <s27343@pjwstk.edu.pl>
2026-03-30 00:41:13 +0200

    Add services for equipment and users menagement

---
+++ APBD-CW2/Services/EquipmentService.cs
@@ -0,0 +1,44 @@
+﻿using APBD_CW2.Enums;
+using APBD_CW2.Models;
+
+namespace APBD_CW2.Services;
+
+public class EquipmentService
+{
+    private readonly List<Equipment> _equipmentList = new();
+
+    public void AddEquipment(Equipment equipment)
+    {
+        if (equipment == null)
+            throw new ArgumentNullException(nameof(equipment));
+
+        _equipmentList.Add(equipment);
+    }
+
+    public List<Equipment> GetAllEquipment()
+    {
+        return _equipmentList;
+    }
+
+    public List<Equipment> GetAvailableEquipment()
+    {
+        return _equipmentList
+            .Where(e => e.Status == EquipmentStatus.Available)
+            .ToList();
+    }
+
+    public Equipment? GetEquipmentById(int id)
+    {
+        return _equipmentList.FirstOrDefault(e => e.Id == id);
+    }
+
+    public void MarkAsUnavailable(int equipmentId)
+    {
+        Equipment? equipment = GetEquipmentById(equipmentId);
+
+        if (equipment == null)
+            throw new InvalidOperationException("Equipment not found.");
+
+        equipment.MarkAsUnavailable();
+    }
+}
---
+++ APBD-CW2/Services/UserService.cs
@@ -0,0 +1,26 @@
+﻿using APBD_CW2.Models;
+
+namespace APBD_CW2.Services;
+
+public class UserService
+{
+    private readonly List<User> _users = new();
+
+    public void AddUser(User user)
+    {
+        if (user == null)
+            throw new ArgumentNullException(nameof(user));
+
+        _users.Add(user);
+    }
+
+    public List<User> GetAllUsers()
+    {
+        return _users;
+    }
+
+    public User? GetUserById(int id)
+    {
+        return _users.FirstOrDefault(u => u.Id == id);
+    }
+}
